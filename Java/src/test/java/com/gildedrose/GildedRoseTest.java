ad5ba44f6f6bc4e7c2930f5ebe0f62275f5d21f6
Bakari Nouhou <bakari.nouhou@lacombedulionvert.fr>
2019-10-17 15:00:21 +0200

    add test items_state_after_one_day

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,17 +1,18 @@
 package com.gildedrose;
 
-import static org.assertj.core.api.Assertions.*;
-import static org.junit.Assert.*;
-
-import org.assertj.core.api.Assertions;
 import org.assertj.core.api.SoftAssertions;
+import org.junit.Before;
 import org.junit.Test;
 
 public class GildedRoseTest {
 
-    @Test
-    public void initial_items_state() {
-        Item[] items = new Item[]{
+    private Item[] items;
+    private GildedRose app;
+
+    @Before
+    public void setUp() {
+
+        items = new Item[]{
                 new Item("+5 Dexterity Vest", 10, 20), //
                 new Item("Aged Brie", 2, 0), //
                 new Item("Elixir of the Mongoose", 5, 7), //
@@ -21,7 +22,11 @@ public class GildedRoseTest {
                 new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
                 new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49)
         };
-        GildedRose app = new GildedRose(items);
+        app = new GildedRose(items);
+    }
+
+    @Test
+    public void initial_items_state() {
         SoftAssertions assertions = new SoftAssertions();
 
         assertions.assertThat(app.items).extracting("name").containsExactly(
@@ -39,6 +44,31 @@ public class GildedRoseTest {
         );
         assertions.assertThat(app.items).extracting("quality").containsExactly(
                 20, 0, 7, 80, 80, 20, 49, 49
+        );
+        assertions.assertAll();
+    }
+
+    @Test
+    public void items_state_after_one_day() {
+        app.updateQuality();
+
+        SoftAssertions assertions = new SoftAssertions();
+
+        assertions.assertThat(app.items).extracting("name").containsExactly(
+                "+5 Dexterity Vest",
+                "Aged Brie",
+                "Elixir of the Mongoose",
+                "Sulfuras, Hand of Ragnaros",
+                "Sulfuras, Hand of Ragnaros",
+                "Backstage passes to a TAFKAL80ETC concert",
+                "Backstage passes to a TAFKAL80ETC concert",
+                "Backstage passes to a TAFKAL80ETC concert"
+        );
+        assertions.assertThat(app.items).extracting("sellIn").containsExactly(
+                9, 1, 4, 0, -1, 14, 9, 4
+        );
+        assertions.assertThat(app.items).extracting("quality").containsExactly(
+                19, 1, 6, 80, 80, 21, 50, 50
         );
         assertions.assertAll();
     }
