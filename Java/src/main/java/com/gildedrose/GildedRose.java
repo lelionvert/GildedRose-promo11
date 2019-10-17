e67e7ceeaf46555fc4b405cfdc6235052900bf5b
Bouchra & Talia Team <essaghir.bouchra@gmail.com>
2019-10-17 17:26:32 +0200

    Loop over item objects instead of item index

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -8,38 +8,38 @@ class GildedRose {
     }
 
     public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            if (items[i].isSulfuras()) {
+        for (Item item : items) {
+            if (item.isSulfuras()) {
                 // doNothing
             }
 
-            else if (items[i].isAgedBrie()) {
-                items[i].increaseQuality();
-                items[i].decreaseSellIn();
-                if (items[i].sellIn < 0) {
-                    items[i].increaseQuality();
+            else if (item.isAgedBrie()) {
+                item.increaseQuality();
+                item.decreaseSellIn();
+                if (item.sellIn < 0) {
+                    item.increaseQuality();
                 }
             }
 
-            else if (items[i].isBackstage()) {
-                items[i].increaseQuality();
-                if (items[i].sellIn < 11) {
-                    items[i].increaseQuality();
+            else if (item.isBackstage()) {
+                item.increaseQuality();
+                if (item.sellIn < 11) {
+                    item.increaseQuality();
                 }
-                if (items[i].sellIn < 6) {
-                    items[i].increaseQuality();
+                if (item.sellIn < 6) {
+                    item.increaseQuality();
                 }
-                items[i].decreaseSellIn();
-                if (items[i].sellIn < 0) {
-                    items[i].resetQuality();
+                item.decreaseSellIn();
+                if (item.sellIn < 0) {
+                    item.resetQuality();
                 }
             }
 
             else {
-                items[i].decreaseQuality();
-                items[i].decreaseSellIn();
-                if (items[i].sellIn < 0) {
-                    items[i].decreaseQuality();
+                item.decreaseQuality();
+                item.decreaseSellIn();
+                if (item.sellIn < 0) {
+                    item.decreaseQuality();
                 }
             }
         }
